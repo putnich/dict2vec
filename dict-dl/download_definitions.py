1366fdeb3918cfb6bda43058130cad96df162c9f
putnich <mputnikovic@gmx.us>
2020-12-17 11:05:31 -0800

    Adapts defs downloader to work with Serbian dict.

--- dict-dl/download_definitions.py
+++ dict-dl/download_definitions.py
@@ -19,7 +19,7 @@
 # You should have received a copy of the GNU General Public License
 # along with Dict2vec.  If not, see <http://www.gnu.org/licenses/>.
 
-from queue import Queue
+from multiprocessing import Queue
 from threading import Thread, Lock
 from multiprocessing import cpu_count
 from downloader import *
@@ -31,15 +31,14 @@ import sys
 # global variables used (and shared) by all ThreadDown instances
 exitFlag = 0
 counterLock = Lock()
-request_counter  = {"Cam": 0, "Dic": 0, "Col": 0, "Oxf": 0}
-download_counter = {"Cam": 0, "Dic": 0, "Col": 0, "Oxf": 0}
+request_counter  = {"Ors": 0}
+download_counter = {"Ors": 0}
 
 class ThreadDown(Thread):
     """Class representing a thread that download definitions."""
-    def __init__(self, dict_name, pos, data_queue, res_queue):
+    def __init__(self, dict_name, data_queue, res_queue):
         Thread.__init__(self)
         self.dict_name  = dict_name
-        self.pos        = pos # part of speech (noun, verb, adjective or all)
         self.data_queue = data_queue
         self.res_queue  = res_queue
 
@@ -47,8 +46,7 @@ class ThreadDown(Thread):
         while not exitFlag:
             if not self.data_queue.empty():
                 word = self.data_queue.get()
-                result = download_word_definition(self.dict_name, word,
-                                                  self.pos)
+                result = download_word_definition(self.dict_name, word)
                 counterLock.acquire()
                 request_counter[self.dict_name] += 1
                 counterLock.release()
@@ -87,7 +85,7 @@ class ThreadWrite(Thread):
 
         self.of.close()
 
-def main(filename, pos="all"):
+def main(filename):
     # 0. to measure download time; use `global` to be able to modify exitFlag
     globalStart = time.time()
     global exitFlag
@@ -103,15 +101,11 @@ def main(filename, pos="all"):
     print("Vocabulary size:", vocabulary_size)
 
     # add "-definitions" before the file extension to create output filename.
-    # If pos is noun/verb/adjective, add it also to the output filename
-    if pos in ["noun", "verb", "adjective"]:
-        output_fn = splitext(filename)[0] + "-definitions-{}.txt".format(pos)
-    else:
-        output_fn = splitext(filename)[0] + "-definitions.txt"
+    output_fn = splitext(filename)[0] + "-definitions.txt"
 
     # look if some definitions have already been downloaded. If that's the case,
     # add the words present in output_fn in the aleady_done variable
-    already_done = {"Cam": set(), "Dic": set(), "Col": set(), "Oxf": set()}
+    already_done = {"Ors": set()}
     reusing = False
     if isfile(output_fn): # need to read the file, first test if it exists
         with open(output_fn) as f:
@@ -133,22 +127,13 @@ def main(filename, pos="all"):
     # 2. create queues containing all words to fetch (1 queue per dictionary)
     # The words to download are in queue_{Cam, Dic, Col, Oxf}, the downloaded
     # definitions are pushed in queue_msg
-    queue_Cam = Queue() # no init val -> infinite size
-    queue_Dic = Queue()
-    queue_Col = Queue()
-    queue_Oxf = Queue()
+    queue_Ors = Queue() # no init val -> infinite size
     queue_msg = Queue()
 
     # only add words in queue if they are not already done
     for w in vocabulary:
-        if not w in already_done["Cam"]:
-            queue_Cam.put(w)
-        if not w in already_done["Dic"]:
-            queue_Dic.put(w)
-        if not w in already_done["Col"]:
-            queue_Col.put(w)
-        if not w in already_done["Oxf"]:
-            queue_Oxf.put(w)
+        if not w in already_done["Ors"]:
+            queue_Ors.put(w)
 
     # 3. create threads
     threads = []
@@ -164,27 +149,15 @@ def main(filename, pos="all"):
 
     # start all the download threads
     for x in range(NB_THREAD):
-        thread_Cam = ThreadDown("Cam", pos, queue_Cam, queue_msg)
-        thread_Cam.start()
-        thread_Dic = ThreadDown("Dic", pos, queue_Dic, queue_msg)
-        thread_Dic.start()
-        thread_Col = ThreadDown("Col", pos, queue_Col, queue_msg)
-        thread_Col.start()
-        thread_Oxf = ThreadDown("Oxf", pos, queue_Oxf, queue_msg)
-        thread_Oxf.start()
+        thread_Ors = ThreadDown("Ors", queue_Ors, queue_msg)
+        thread_Ors.start()
 
-        threads.append(thread_Cam)
-        threads.append(thread_Dic)
-        threads.append(thread_Col)
-        threads.append(thread_Oxf)
+        threads.append(thread_Ors)
 
     # 4. wait until all queues are empty, show progress, terminate all threads
     percent = 0
     while not queue_msg.empty() or \
-          not queue_Cam.empty() or \
-          not queue_Dic.empty() or \
-          not queue_Col.empty() or \
-          not queue_Oxf.empty():
+          not queue_Ors.empty():
 
         # the expected number of fetched words is the number of words
         # in the vocabulary times 4 because we are using 4 dictionaries.
@@ -192,7 +165,7 @@ def main(filename, pos="all"):
         tmp = sum(request_counter.values()) / (4.0 * vocabulary_size) * 100
         tmp = int(tmp) + 1
         if tmp != percent:
-            print('\r{0}%'.format(tmp), end="")
+            print('\r{0}%'.format(tmp))
             percent = tmp
 
     exitFlag = 1
@@ -210,8 +183,7 @@ def main(filename, pos="all"):
     print("==============================================")
     for dic in sorted(request_counter.keys()):
         print("{}   {}/{}".format(
-              dic, download_counter[dic], request_counter[dic]),
-              end="")
+              dic, download_counter[dic], request_counter[dic]))
         if (request_counter[dic] > 0): # so no division by zero
             print("  ({:.1f}%)".format(
                 download_counter[dic] * 100 / request_counter[dic]))
@@ -225,15 +197,6 @@ if __name__ == '__main__':
     parser.add_argument("list_words", metavar="list-words",
         help="""File containing a list of words (one per line). The script will
         download the definitions for each word.""")
-    parser.add_argument("-pos", help="""Either NOUN/VERB/ADJECTIVE. If POS (Part
-        Of Speech) is given, the script will only download the definitions that
-        corresponds to that POS, not the other ones. By default, it downloads
-        the definitions for all POS""", type=str.lower, default="all")
     args = parser.parse_args()
 
-    if args.pos not in ["noun", "verb", "adjective", "all"]:
-        print("WARNING: invalid POS argument \"{}\"".format(args.pos))
-        print("It can be NOUN, VERB or ADJECTIVE. Using default POS (ALL)\n")
-        args.pos = "all"
-
-    main(args.list_words, pos=args.pos)
+    main(args.list_words)
--- dict-dl/downloader.py
+++ dict-dl/downloader.py
@@ -19,348 +19,61 @@
 # You should have received a copy of the GNU General Public License
 # along with Dict2vec.  If not, see <http://www.gnu.org/licenses/>.
 
-from urllib.error import HTTPError
-import urllib.request
 import re
+import requests
+from bs4 import BeautifulSoup
 
-def download_cambridge(word, pos="all"):
-    URL = "http://dictionary.cambridge.org/dictionary/english/" + word
 
-    if pos not in ["all", "adjective", "noun", "verb"]:
-        pos = "all"
+def download_online_recnik(word):
+    URL = 'https://recnik.rs/leksikon/srpski/' + word
 
     try:
-        html = urllib.request.urlopen(URL).read().decode('utf-8')
-
-        # definitions are in a <b> tag that has the class "def"
-        defs_pat = re.compile('<b class="def">(.*?)</b>', re.I|re.S)
-
-        # need to extract definitions only if it's a certain pos type
-        if pos in ["adjective", "noun", "verb"]:
-
-            # each type entry (adj, noun or verb) is in a "entry-body__el"
-            # block. A word might have many blocks (if it is both a noun and a
-            # verb, it will have 2 blocks). Moreover, there are also different
-            # blocks for British or American language. I can't extract blocks
-            # because there is no ending regex that works for every word, so I
-            # consider a block to be between the indexes of 2 consecutive
-            # block_pat matches. Last block goes to the end of html string.
-            block_pat = re.compile('<div class="entry-body__el ', re.I|re.S)
-            idx = [m.start() for m in block_pat.finditer(html)] + [len(html)]
-            span = [(idx[i], idx[i+1]) for i in range(len(idx)-1)]
-
-            # then for each block, I only extract the definitions if it matches
-            # the pos argument
-            pos_pat = re.compile('class="pos".*?>(.*?)</span>', re.I|re.S)
-            defs = []
-
-            for start, end in span:
-                pos_extracted = re.search(pos_pat, html[start:end])
-
-                # some words (like mice) do not have a pos info, so no pos
-                # extracted
-                if pos_extracted is None:
-                    continue
-
-                pos_extracted = pos_extracted.group(1)
-
-                if pos_extracted != pos:
-                    continue
-
-                defs += re.findall(defs_pat, html[start:end])
-
-        # otherwise extract all definitions available
-        else:
-            defs = re.findall(defs_pat, html)
-
-        # need to clean definitions of <a> and <span> tags. Use cleaner to
-        # replace these tags by empty string
-        cleaner = re.compile('<.+?>', re.I|re.S)
-        return [ re.sub(cleaner, '', x) for x in defs ]
-
-    except HTTPError:
-        return -1
+        html_page = requests.get(URL)
+        soup = BeautifulSoup(html_page.content, 'html.parser')
+        defs = []
+        for h3 in soup.find('div', {'id': 'main'}).find_all('h3'):
+            defs.append(h3.find_next_sibling().text)
+        return defs
     except UnicodeDecodeError:
         return -1
     except Exception as e:
         print("\nERROR: * timeout error.")
-        print("       * retry Cambridge -", word)
-        return -1
-
-def download_dictionary(word, pos="all"):
-    URL = "http://www.dictionary.com/browse/" + word
-
-    if pos not in ["all", "adjective", "noun", "verb"]:
-        pos = "all"
-
-    try:
-        html = urllib.request.urlopen(URL).read().decode('utf-8')
-
-        # definitions are in <section> tags with class "css-171jvig". Each POS
-        # type has its own <section>, so extract them all.
-        block_pat = re.compile('<section class="css-171jvig(.*?)</section>',
-                               re.I|re.S)
-        blocks = re.findall(block_pat, html)
-
-        # inside each block, definitions are in <span> tags with the class
-        # "css-1e3ziqc". Sometimes there is another class, so use the un-greedy
-        # regex pattern .+? to go until the closing '>' of the opening <span>
-        # tag.
-        defs_pat = re.compile('<span class=".+?css-1e3ziqc.+?>(.*?)</span>', re.I|re.S)
-
-        # need to extract definitions only if it's a certain pos type
-        if pos in ["adjective", "noun", "verb"]:
-
-            # for each block, if the extracted POS matches the pos argument, add
-            # the definitions in defs (.+ because class is either luna-pos or
-            # pos)
-            pos_pat = re.compile('class=.+pos">(.*?)</span>', re.I|re.S)
-            defs = []
-
-            for block in blocks:
-                pos_extracted = re.search(pos_pat, block)
-
-                # some words (like cia) do not have a pos info so no pos
-                # extracted
-                if pos_extracted is None:
-                    continue
-
-                pos_extracted = pos_extracted.group(1)
-
-                if pos not in pos_extracted:
-                    continue
-
-                # remove possible sentence examples in definitions and possible
-                # non informative labels (like "Archaic" in one of the
-                # definition of "wick")
-                defs += [ re.sub('<span class="luna-example.+$', '', x)
-                          for x in re.findall(defs_pat, block)
-                          if "luna-label" not in x ]
-
-        # otherwise, concatenate all blocks and extract all definitions
-        # available. Remove possible sentence examples in definitions and
-        # possible non informative labels (like "Archaic" in one of the
-        # definition of "wick")
-        else:
-            defs = re.findall(defs_pat, " ".join(blocks))
-            defs = [ re.sub('<span class="luna-example.+$', '', x)
-                     for x in defs if "luna-label" not in x]
-
-        # need to clean definitions of <span> tags. Use cleaner to replace these
-        # tags by empty string, Use .strip() to also clean some \r or \n.
-        cleaner = re.compile('<.+?>', re.I|re.S)
-        return [ re.sub(cleaner, '', x).strip() for x in defs ]
-
-    except HTTPError:
-        return -1
-    except UnicodeDecodeError:
-        return -1
-    except IndexError:
-        return -1
-    except Exception as e:
-        print("\nERROR: * timeout error.")
-        print("       * retry dictionary.com -", word)
-        return -1
-
-def download_collins(word, pos="all"):
-    URL = "https://www.collinsdictionary.com/dictionary/english/" + word
-
-    # Collins has set some server restrictions. Need to spoof the HTTP headers
-    headers = {
-        'User-Agent':
-            'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like '
-            'Gecko) Chrome/23.0.1271.64 Safari/537.11',
-        'Accept':
-            'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-        'Accept-Encoding':
-            'none',
-        'Accept-Language':
-            'en-US,en;q=0.8',
-        }
-    req = urllib.request.Request(URL, headers=headers)
-
-    if pos not in ["all", "adjective", "noun", "verb"]:
-        pos = "all"
-
-    try:
-        html = urllib.request.urlopen(req).read().decode('utf-8')
-
-        # definitions are in big blocks <div class="content definitions [...] >
-        # Use the next <div> with "copyright" for ending regex. Regroup all
-        # blocks.
-        block_p = re.compile('<div class="content definitions.+?"(.*?)'
-                             '<div class="div copyright', re.I|re.S)
-        blocks = " ".join(re.findall(block_p, html))
-
-        # inside this block, definitions are in <div class="def">...</div>
-        defs_pat = re.compile('<div class="def">(.+?)</div>', re.I|re.S)
-
-        # need to extract definitions only if it's a certain pos type
-        if pos in ["adjective", "noun", "verb"]:
-
-            # each sense of the word is inside a <div class="hom">. Get all the
-            # starting and ending indexes of these blocks
-            sense_pat = re.compile('<div class="hom">', re.I| re.S)
-            idx = [m.start() for m in sense_pat.finditer(blocks)]
-            idx.append(len(blocks))
-            span = [(idx[i], idx[i+1]) for i in range(len(idx)-1)]
-
-            # then for each sense, I only extract the definitions if it matches
-            # the pos argument
-            pos_pat = re.compile('class="pos">(.*?)</span>', re.I|re.S)
-            defs = []
-
-            for start, end in span:
-                pos_extracted = re.search(pos_pat, blocks[start:end])
-                # sometimes, sense is just a sentence or an idiom, so no pos
-                # extracted
-                if pos_extracted is None:
-                    continue
-
-                # noun is sometimes written as "countable noun". "verb" as
-                # "verb transitive". Use the `in` trick to match these 2
-                # categories with either "noun" or "verb"
-                pos_extracted = pos_extracted.group(1)
-
-                if pos not in pos_extracted:
-                    continue
-
-                defs += re.findall(defs_pat, blocks[start:end])
-
-        # otherwise extract all definitions available
-        else:
-            defs = re.findall(defs_pat, blocks)
-
-        # need to clean definitions of <a> and <span> tags. Use cleaner to
-        # replace these tags by empty string, Use .strip() to also clean some
-        # \r or \n, and replace because sometimes there are \n inside a sentence
-        cleaner = re.compile('<.+?>', re.I|re.S)
-        return [re.sub(cleaner, '', x).replace('\n', ' ').strip() for x in defs]
-
-    except HTTPError:
-        return -1
-    except UnicodeDecodeError:
-        return -1
-    except IndexError:
-        return -1
-    except Exception as e:
-        print("\nERROR: * timeout error.")
-        print("       * retry Collins -", word)
-        return -1
-
-def download_oxford(word, pos="all"):
-    URL = "http://en.oxforddictionaries.com/definition/"+ word
-
-    if pos not in ["all", "adjective", "noun", "verb"]:
-        pos = "all"
-
-    try:
-        html = urllib.request.urlopen(URL).read().decode('utf-8')
-
-        # extract blocks containing POS type and definitions. For example, if
-        # word is both a noun and a verb, there is one <section class="gramb">
-        # block for the noun definitions, and another for the verb definitions
-        block_p = re.compile('<section class="gramb">(.*?)</section>', re.I|re.S)
-        blocks  = re.findall(block_p, html)
-
-        # inside these blocks, definitions are in <span class="ind">
-        defs_pat = re.compile('<span class="ind">(.*?)</span>', re.I|re.S)
-
-        # need to extract definitions only if it's a certain pos type
-        if pos in ["adjective", "noun", "verb"]:
-
-            # for each block, I only extract the definitions if it matches the
-            # pos argument
-            pos_pat = re.compile('class="pos">(.*?)</span>', re.I|re.S)
-            defs = []
-
-            for block in blocks:
-                pos_extracted = re.search(pos_pat, block).group(1)
-
-                if pos_extracted != pos:
-                    continue
-
-                defs += re.findall(defs_pat, block)
-
-        # otherwise extract all definitions available
-        else:
-            defs = re.findall(defs_pat, "".join(blocks))
-
-        # need to clean definitions of <a> and <span> tags. Use cleaner to
-        # replace these tags by empty string
-        cleaner = re.compile('<.+?>', re.I|re.S)
-        return [ re.sub(cleaner, '', x) for x in defs ]
-
-    except HTTPError:
-        return -1
-    except UnicodeDecodeError:
-        return -1
-    except IndexError:
-        return -1
-    except Exception as e:
-        print("\nERROR: * timeout error.")
-        print("       * retry Oxford -", word)
+        print("       * retry Online recnik -", word)
         return -1
 
 MAP_DICT = {
-    "Cam": download_cambridge,
-    "Dic": download_dictionary,
-    "Col": download_collins,
-    "Oxf": download_oxford,
+    "Ors": download_online_recnik
 }
 
-STOPSWORD = set()
-with open('stopwords.txt') as f:
-    for line in f:
-        STOPSWORD.add(line.strip().lower())
+# STOPSWORD = set()
+# with open('stopwords.txt') as f:
+#     for line in f:
+#         STOPSWORD.add(line.strip().lower())
 
-def download_word_definition(dict_name, word, pos="all", clean=True):
+
+def download_word_definition(dict_name, word, clean=True):
     """
     Download the definition(s) for word from the dictionary dict_name. If clean
     is True, clean the definition before returning it (remove stopwords and non
     letters characters in definition).
     """
-    words    = []
+    words = []
     download = MAP_DICT[dict_name]
-    res      = download(word, pos=pos)
-    if res == -1: # no definition fetched
+    res = download(word)
+    if res == -1:  # no definition fetched
         res = []
 
-    for definition in res: # there can be more than one definition fetched
-        # if no cleaning needed, add the whole definition
-        if not clean:
-            words.append(definition)
-            continue
-
+    for definition in res:  # there can be more than one definition fetched
         for word in definition.split():
             word = ''.join([c.lower() for c in word
-                         if c.isalpha() and ord(c) < 128])
-            if not word in STOPSWORD:
-                words.append(word)
+                            if c.isalpha()])
+            # if not word in STOPSWORD:
+            words.append(word)
 
     return words
 
-if __name__ == '__main__':
-    print("-- TEST : definitions of wick --")
-    print("Cambridge")
-    print(download_cambridge("wick", "all"))
-    print("\ndictionary.com")
-    print(download_dictionary("wick", "all"))
-    print("\nCollins")
-    print(download_collins("wick", "all"))
-    print("\nOxford")
-    print(download_oxford("wick", "all"))
 
-    print("\n\n-- TEST : definitions according to POS of alert --")
-    print("dictionary.com -- alert [ADJECTIVE]")
-    print(download_dictionary("alert", "adjective"))
-    print()
-    print("dictionary.com -- alert [NOUN]")
-    print(download_dictionary("alert", "noun"))
-    print()
-    print("dictionary.com -- alert [VERB]")
-    print(download_dictionary("alert", "verb"))
-    print()
-    print("dictionary.com -- alert [ALL]")
-    print(download_dictionary("alert", "all"))
-    print()
+if __name__ == '__main__':
+    print("-- TEST : definitions of kosa --")
+    print("Online recnik")
+    print(download_online_recnik("kosa"))
